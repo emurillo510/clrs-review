dd684c6f77ec50341df780de16e6a9c100450454
Elton Murillo <emurillo510@gmail.com>
2016-09-27 06:09:53 -0700

    Bubblesort boilerplate and some pseudocode

---
+++ Bubblesort.java
@@ -0,0 +1,25 @@
+public class Bubblesort {
+
+  public static void main(String[]args) { 
+
+
+
+
+  }
+
+  // 10,9,8,7,6,5,4,3,2,1
+  public List<Integer> bubbleSort(int[] numbers, int n) {
+
+    // while list is not sorted
+
+    // if the current index i is less than the next number i + 1
+
+    // swap the two numbers
+
+    // increment i
+
+    // if no swap at the end of the loop occured the list is now sorted
+
+  }
+
+}
