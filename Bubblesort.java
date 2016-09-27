5fe8eb19bc57c7780487a421f23fd3246771958d
Elton Murillo <emurillo510@gmail.com>
2016-09-27 06:36:44 -0700

    Bubblesort in java

--- Bubblesort.java
+++ Bubblesort.java
@@ -1,24 +1,56 @@
+import java.util.List;
+import java.util.Arrays;
+
 public class Bubblesort {
 
   public static void main(String[]args) { 
 
+    int[] unsorted = { 10,9,8,7,6,5,4,3,2,1 };
+    int size = unsorted.length-1;
 
+    final int[] sorted = bubbleSort(unsorted,size);
 
-
+    for(int i: sorted) {
+      System.out.print(i + ",");
+    }
   }
 
   // 10,9,8,7,6,5,4,3,2,1
-  public List<Integer> bubbleSort(int[] numbers, int n) {
+  public static int[] bubbleSort(int[] numbers, int n) {
 
-    // while list is not sorted
 
-    // if the current index i is less than the next number i + 1
+    // while list is not sorted (if swapped is true)
+    while(true) {
 
-    // swap the two numbers
+      boolean swapped = false;
 
-    // increment i
+      for(int i=0; i < numbers.length-1; i++) {
 
-    // if no swap at the end of the loop occured the list is now sorted
+        if(numbers[i] > numbers [i+1]) {
+          numbers = swap(numbers,i,i+1);
+          swapped = true;
+        }
+
+      }
+
+      if(swapped == false) {
+        break;
+      }
+    }
+
+    return numbers;
+
+  }
+
+  public static int[] swap(int[]list, int indexA, int indexB) {
+
+    int temp = list[indexB];
+
+    list[indexB] = list[indexA];
+    list[indexA] = temp;
+
+    return list;
+
 
   }
 
